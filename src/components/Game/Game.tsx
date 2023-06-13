2e0435a0bb6bffef844245286ebd9a5f9fd974ce
PAUL HARDMAN <89204135+singapaul@users.noreply.github.com>
2023-06-13 13:59:01 +0100

    remove unnessary import

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
-import { ReactNode } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Card from "../Card";
